556f88f8663e2bed558006d149787448fc3053fd
shubhamranjan12 <shubhamranjan95@gmail.com>
2021-12-26 18:37:32 +0530

    Extending-Ansible: fix lookup plugin

--- lookup_plugins/hostip.py
+++ lookup_plugins/hostip.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 # Written By - Rishabh Das <rishabh5290@gmail.com>
-#
 # This program is a free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation; either version 2 of the license, or(at your option) any
@@ -12,8 +11,9 @@
 
 import socket
 
+from ansible.plugins.lookup import LookupBase
 
-class LookupModule(object):
+class LookupModule(LookupBase):
 
     '''
     Returns IP of host
@@ -22,9 +22,11 @@ class LookupModule(object):
     def __init__(self, basedir=None, **kwargs):
         self._basedir = basedir
 
-    def run(self, hostname, **kwargs):
-        try:
-            host_detail = socket.gethostbyname(hostname)
-        except:
-            host_detail = 'Invalid Hostname'
+    def run(self, hostname, inject=None, **kwargs):
+        host_detail = []
+        for h in hostname:
+            try:
+                host_detail.append(socket.gethostbyname(h))
+            except:
+                host_detail.append('Invalid Hostname')
         return host_detail
